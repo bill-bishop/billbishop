a97bcd4063335b6ec800cbfd00b5b49898de8d5e
bill <bill@bill.bill>
2024-11-25 16:00:26 -0700

    added mercy's page

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // index.js
 
 import http from 'http-server';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = process.env.PORT || 8080;
 
-const server = http.createServer({ root: '.' });
+const server = http.createServer({ root: path.join(__dirname, 'public') });
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
